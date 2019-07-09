6196a01a114ff03b3f6b121a5243341146a9b97e
Naveen_Kumar24 <51779899+naveenkumar2405@users.noreply.github.com>
2019-07-09 12:27:52 +0530

    Add files via upload

---
+++ CK4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	char ch;
+	cin >> ch;
+	if((ch>='a'&& ch<='z') || (ch>='A' && ch<='Z'))
+	{
+		cout <<"Alphabet";
+	}
+	else
+	{
+		cout <<"No";
+	}
+}
