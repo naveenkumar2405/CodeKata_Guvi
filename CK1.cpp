1957eedaac22b49b0c58b9b1ce956827471d9f21
Naveen_Kumar24 <51779899+naveenkumar2405@users.noreply.github.com>
2019-07-09 10:07:42 +0530

    Update CK1.cpp

--- CK1.cpp
+++ CK1.cpp
@@ -8,7 +8,7 @@ int main ()
     if (num > 0)
         cout << "positive";
     else if (num<0)
-        cout << num << "negative";
+        cout << "negative";
         else
             cout << "invalid";
     return 0;
